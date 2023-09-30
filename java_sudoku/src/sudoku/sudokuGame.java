fb9c4fe6c7418aaaac264346e4901f5f8520ab69
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-30 16:58:15 -0500

    create constructor of sudokuGame

--- java_sudoku/src/sudoku/sudokuGame.java
+++ java_sudoku/src/sudoku/sudokuGame.java
@@ -1,5 +1,16 @@
 package sudoku;
 
 public class sudokuGame {
-
+	private final GameState gameState;
+	private final int[][] gridState;
+	
+	public static final GRID_BOUNDARY = 9;
+	
+	public sudokuGame(GameState gameState, int[][] gridState) {
+		this.gameState = gameState;
+		this.gridState = gridState;
+	}
+	
+	
+	
 }
