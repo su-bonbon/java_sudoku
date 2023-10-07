32d9acc1aac2dd6b04b002ed5c73368948a814e3
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-07 12:41:57 -0500

    create equals method

--- java_sudoku/src/sudoku/Coordinates.java
+++ java_sudoku/src/sudoku/Coordinates.java
@@ -7,6 +7,19 @@ public class Coordinates {
 	public Coordinates(int x, int y) {
 		this.x = x;
 		this.y = y;
-		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(this != o) return false;
 	}
 }
