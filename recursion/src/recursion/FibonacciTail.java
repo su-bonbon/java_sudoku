51dbc1e675a24cf8e93027463053babb7866847c
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-11 19:47:24 -0500

    modify fibonacci as tail recursion way

--- recursion/src/recursion/FibonacciTail.java
+++ recursion/src/recursion/FibonacciTail.java
@@ -3,20 +3,22 @@ package recursion;
 public class FibonacciTail {
 	
 	public static long fib(int n){
-		if (n <= 2) {
-	        return 1;
-	    }
+
 		//System.out.println("enter1");
 		return fib(n, 0, 1);
+		
 	}
 	
 	public static long fib(int n, int i, int j){
 		//System.out.println("enter2");
-		if (n > 0) {
-			return fib(n-1, j, i+j);
-		} else {
+		if (n == 0) {
 			return i;
+		} else if (n == 1) { 
+			return j;
+		} else {
+			return fib(n-1, j, i+j);
 		}
+		
 	}
 	
 	public static void main(String[] args) {
