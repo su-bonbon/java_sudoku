a7f4d02d64adfc9f500a9f92ba983811f6aacae0
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-20 16:50:51 -0500

    create massage class

--- java_sudoku/src/sudoku/constants/Messages.java
+++ java_sudoku/src/sudoku/constants/Messages.java
@@ -1,5 +1,6 @@
 package sudoku.constants;
 
 public class Messages {
-
+	public static final String GAME_CPMPLETE = "Congrateulations, you have won! New Game?";
+	public static final String ERROR = "An Error has occured";
 }
