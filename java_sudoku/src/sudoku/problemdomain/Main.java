d7c87f413f57ae19b2ac3cc7059602a15d1ba9b8
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-21 22:42:55 -0500

    create main class

--- java_sudoku/src/sudoku/problemdomain/Main.java
+++ java_sudoku/src/sudoku/problemdomain/Main.java
@@ -3,7 +3,7 @@ package sudoku.problemdomain;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SudokuApplication.main(new String[] {});
 
 	}
 
